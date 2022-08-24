56e53e189d39069db81d2909d8a7656dd6ab3dc9
Stepan Rudenko <s.rudenko584@gmail.com>
2022-08-24 13:56:26 +0300

    fixed cors error and added additional route for getting tags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -138,3 +138,21 @@ export const updateOne = async (req, res) => {
     });
   }
 };
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не вдалося отримати теги",
+    });
+  }
+};
